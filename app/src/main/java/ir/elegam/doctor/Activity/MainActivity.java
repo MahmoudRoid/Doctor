d7c83149300cdc0a8c38c359216714e78e020603
Mahmoud <m.mahmoudreza1@yahoo.com>
2016-08-17 10:32:44 +0430

    dr initial

--- app/src/main/java/ir/elegam/doctor/Activity/MainActivity.java
+++ app/src/main/java/ir/elegam/doctor/Activity/MainActivity.java
@@ -13,6 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(MainActivity.this,ServicesActivity.class));
     }
 }
